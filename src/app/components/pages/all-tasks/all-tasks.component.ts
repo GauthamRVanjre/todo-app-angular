dbdc089682688d2142660a40206ab1bd275d7932
Gautham Vanjre <vanjregautham@gmail.com>
2024-07-28 16:28:55 +0530

    feat: add task, get all tasks service and UI

--- src/app/components/pages/all-tasks/all-tasks.component.ts
+++ src/app/components/pages/all-tasks/all-tasks.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpService } from '../../../services/http.service';
 
 @Component({
   selector: 'app-all-tasks',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './all-tasks.component.html',
-  styleUrl: './all-tasks.component.scss'
+  styleUrl: './all-tasks.component.scss',
 })
 export class AllTasksComponent {
+  newTask = '';
+  tasksList = [];
+  httpService = inject(HttpService);
 
+  ngOnInit() {
+    this.getAllTasks();
+  }
+  addTask() {
+    console.log('Adding task', this.newTask);
+    this.httpService.addTask(this.newTask).subscribe(() => {
+      this.newTask = '';
+    });
+  }
+  getAllTasks() {
+    this.httpService.getAllTasks().subscribe((res: any) => {
+      this.tasksList = res;
+    });
+  }
 }
